245e5531824b9d7fdff7139d0f6f3c0da88ca87d
Ahmed Basuoni <ahmed.yasser.bsuoni@gmail.com>
2023-06-04 17:02:27 +0300

    Finish

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const Agora = require("agora-access-token");
 
 const app = express();
 app.use(express.json());
-app.post("*", (req, res) => {
-  res.send({ message: 'hi Mina' });
-})
+
 app.post("/gettoken", (req, res) => {
   const appID = "c3e8e71cae794f4eb01046f29e2e84e9";
   const appCertificate = "101f2ad714e742ebbf1303cf7bb032f1";
   const expirationTimeInSeconds = 3600;
   const uid = Math.floor(Math.random() * 100000);
-  const role = Agora.RtcRole.PUBLISHER;
+  const role = req.body.isPublisher ? Agora.RtcRole.PUBLISHER : Agora.RtcRole.SUBSCRIBER;
   const channel = req.body.channel;
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const expirationTimestamp = currentTimestamp + expirationTimeInSeconds;
+
   const token = Agora.RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channel, uid, role, expirationTimestamp);
-  console.log({ uid, token });
+ console.log({ uid, token });
   res.send({ uid, token });
 });
+app.get("*", (req, res) => {
+  res.send({ message: 'hi Mina' });
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Agora Auth Token Server listening at Port ${port}`));
